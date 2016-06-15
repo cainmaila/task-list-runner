6255dfa35d22228be0ebe409adbda45965a4306d
Cain.Chu <cainmaila@gmail.com>
2016-06-15 15:21:11 +0800

    駐列處理 v1.0

---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = {
+    list: [],
+    start: function() {
+        if (this.isRuning) return;
+        if (this.list.length > 0) {
+            this.isRuning = true;
+            this.runingTask = this.list.shift();
+            this.next();
+        } else {
+            this.runingTask = null;
+            this.endCall();
+        }
+    },
+    isRuning: false,
+    runingTask: null,
+    nextData: null,
+    next: function() {
+        this.runingTask.tasker = this;
+        this.runingTask.task();
+    },
+    endAndNext: function() {
+        this.endAnTask(this.runingTask.data);
+        this.isRuning = false;
+        this.start();
+    },
+    endAnTask: function(data) {
+        console.warn(data, 'Please override method endAnTask!!');
+    },
+    endCall: function() {
+        console.warn('Please override method endCall!!');
+    },
+    push: function(task) {
+        this.list.push(task);
+        this.start();
+    }
+};
